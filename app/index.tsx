87a85a60cefa59807f3bd219d434fada94673ebe
Utkarsh Singh <kshatriyautkarsh404@gmail.com>
2026-02-21 15:04:53 +0530

    chore: setup production URL and OTA updates

--- app/index.tsx
+++ app/index.tsx
@@ -5,7 +5,7 @@ import WebViewContainer from '../components/WebViewContainer';
 export default function AppIndex() {
     // If testing on a physical device, this should be the IPv4 address of your computer running the Next.js server.
     // Based on your Expo logs, your network IP seems to be 192.168.1.36.
-    const defaultUrl = 'http://192.168.1.36:3000';
+    const defaultUrl = 'https://astroby-ab-sy16.vercel.app/';
     const appUrl = process.env.EXPO_PUBLIC_APP_URL || defaultUrl;
 
     return <WebViewContainer url={appUrl} />;
